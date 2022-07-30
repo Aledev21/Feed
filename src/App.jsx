01955f95cc5494dbcb4873da2cfaf2efa70bc8ed
Alessandro Assis <aledev21@gmail.com>
2022-07-30 07:16:47 -0300

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post" 
+import { Header } from './components/Hearder';
 export function App() {
 
   return (
-            <>
+            <> 
+            <Header />
+
             <Post 
             author="Gabriel"  
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vitae, iure, dicta laudantium ab iusto odit adipisci maiores beatae repellendus, expedita accusantium perspiciatis earum soluta deleniti. Quos autem adipisci eum!"
