8d4940938ef9c6a7a2114281d6d1d6681b8a3cf3
Alessandro Assis <aledev21@gmail.com>
2022-08-05 02:53:46 -0300

    finalizing component sidebar and css module sideba

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,24 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
+      
       <img className= {styles.cover}   src="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"  
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/aledev21.png"  />
+
         <strong>Alessandro Assis</strong>
         <span>FullStack Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
