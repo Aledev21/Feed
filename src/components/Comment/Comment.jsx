88c66594faadf01022148aec60fcc47acffbdf46
Alessandro Assis <aledev21@gmail.com>
2022-09-08 16:34:25 -0300

    fix Comment component

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -4,11 +4,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 
 export function Comment() {
   return (
-    <div className={styles.Comment}>
+    <div className={styles.comment}>
       <img src="https://github.com/aledev21.png" alt="" />
 
-      <div className={styles.CommentBox}>
-        <div className={styles.CommentContent}>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong> Alessandro Assis</strong>
